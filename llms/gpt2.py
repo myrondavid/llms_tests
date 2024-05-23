119bc30a8b27f8eb629ed3891fc5a34c656c54d5
Myron David <mdlcp@ic.ufal.br>
2024-05-23 09:47:08 -0300

    Adding manual test experiments

--- llms/gpt2.py
+++ llms/gpt2.py
@@ -5,6 +5,6 @@ set_seed(42)
 
 #generator("Hello, I'm a language model,", max_length=144, num_return_sequences=1)
 
-def gen_text_by_prompt(text, qnt_sentences, max_length):
+def gen_text_by_prompt(text, qnt_sentences=1, max_length=144):
     return generator(text, max_length=max_length, num_return_sequences=qnt_sentences)
     
